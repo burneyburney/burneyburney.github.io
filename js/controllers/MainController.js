a133f31a5ce8d822d1f8e6bd88a3bb31e4afd2f9
michael burney <burney318@gmail.com>
2016-07-20 19:48:02 -0500

    editing controller ... still broken

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,8 +3,8 @@ app.controller('MainController',[
     $scope.title = 'workingTitle';
     $scope.promo = "We Marked the prices up just to mark them down and call it a sale!";
   $scope.product =[{
-    name: 'The Book of Trees',
+    name: 'RED SOX GIVEAWAY',
     price: 19,
-    pubdate: new Date('2014', '03', '08'),
-    cover: 'img/the-book-of-trees.jpg'}];
-  }]);
+    productDate: new Date('1999', '', ''),
+    img: 'img/the-book-of-trees.jpg'}];
+}]);
