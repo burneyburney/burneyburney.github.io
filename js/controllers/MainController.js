2796678ce72b1129d66d68572bd05b7d98c6b9a1
michael burney <burney318@gmail.com>
2016-07-20 21:13:39 -0500

    small updates, thinking of refactoring to just use ng repeat , just to 'for each' everything onto the screen

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,7 +4,7 @@ app.controller('MainController',[
         $scope.promo = "We Marked the prices up just to mark them down and call it a sale!";
     $scope.product =[{
         name: 'RED SOX GIVEAWAY',
-        price: 19,
+        price: 150,
         productDate: new Date('1999', '00', '01'),
         legoImage: 'http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/red-sox-fig.jpg'
     }];
