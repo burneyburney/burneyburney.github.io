957892b279f719b1641255ebd277b7c059806aa4
michael burney <burney318@gmail.com>
2016-07-20 20:31:17 -0500

    updated... 	README edited 	css edited 	index edited 	MainController edited

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 started -> 6:25pm  Jul 20, 2016  
 
-# starting angular (1.5) project  
+# quick and dirty angular (1.5) project  
 
 ## order of operations
 
@@ -9,11 +9,11 @@ started -> 6:25pm  Jul 20, 2016
 ~~3.  agree on color pallet~~  
 ~~4.  import what you learned from past angular project~~  
 ~~5. build upon past skills to what you know now~~  
+time check in -> 8:30pm   ()   
+    ~~6.  making everything responsive / mobile friendly~~  
 
-    6.  making everything responsive / mobile friendly
     7.  launch onto github pages
     8.  tell EVERYONE what you did... or you might as well have not done it at all.
 
-
-
-ended ->
+ended ->    
+hosted on ->  burneyburney.github.io  
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,10 +1,11 @@
 app.controller('MainController',[
-  '$scope', function($scope){
-    $scope.title = 'workingTitle';
-    $scope.promo = "We Marked the prices up just to mark them down and call it a sale!";
-  $scope.product =[{
-    name: 'RED SOX GIVEAWAY',
-    price: 19,
-    productDate: new Date('1999', '', ''),
-    img: 'img/the-book-of-trees.jpg'}];
+    '$scope', function($scope){
+        $scope.title = 'workingTitle';
+        $scope.promo = "We Marked the prices up just to mark them down and call it a sale!";
+    $scope.product =[{
+        name: 'RED SOX GIVEAWAY',
+        price: 19,
+        productDate: new Date('1999', '1', '1'),
+        legoImage: 'http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/red-sox-fig.jpg'
+    }];
 }]);
