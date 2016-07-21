48d8f5e2c1ec11711a00bccf820419e808633358
michael burney <burney318@gmail.com>
2016-07-20 19:29:34 -0500

    added first item. ISSUE -> cant see controller object

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,8 @@
 app.controller('MainController',[
   '$scope', function($scope){
-    $scope.title = 'CHANGE IT!';
-    $scope.promo = "newPromo?";
-  $scope.products =[{
+    $scope.title = 'workingTitle';
+    $scope.promo = "We Marked the prices up just to mark them down and call it a sale!";
+  $scope.product =[{
     name: 'The Book of Trees',
     price: 19,
     pubdate: new Date('2014', '03', '08'),
